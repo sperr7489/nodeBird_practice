bb3420a6cb714b359047ac4dfd44ba0d6e9f5482
sperr <sperr@ajou.ac.kr>
2021-08-09 13:13:08 +0900

    kakao login update

---
+++ routes/page.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { isLoggedIn, isNotLoggedIn } = require("./middlewares"); //로그인이 되어 있을 때와 로그인이 안 되어 있을 때를 나눈 것.
+
+const router = express.Router();
+
+router.use((req, res, next) => {
+  res.locals.user = req.user;
+  res.locals.followerCount = 0;
+  res.locals.followingCount = 0;
+  res.locals.followerIdList = [];
+  next();
+});
+
+router.get("/profile", isLoggedIn, (req, res) => {
+  res.render("profile", { title: "내 정보 - NodeBird" });
+});
+
+router.get("/join", isNotLoggedIn, (req, res) => {
+  res.render("join", { title: "회원가입 - NodeBird" });
+});
+
+router.get("/", (req, res, next) => {
+  const twits = [];
+  res.render("main", {
+    title: "NodeBird",
+    twits,
+  });
+});
+
+module.exports = router;
